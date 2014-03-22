675f2fef282f3006cd0bda3f39f520ae1002a273
Jesse Connop <jconnop@gmail.com>
2014-03-22 18:04:01 +1300

    Hello Triangle

--- OpenGL_Tutorial_1/main.cpp
+++ OpenGL_Tutorial_1/main.cpp
@@ -35,14 +35,81 @@ int main() {
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
 	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
-	/* OTHER STUFF GOES HERE NEXT */
+	// Triangle Mesh
+	float points[] = {
+		 0.0f,  0.5f,  0.0f,
+		 0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f
+	};
+
+	// Setup vertex array object for triangle
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	// Setup vertex atrtibute object for trinangle
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// Shaders
+	const char* vertex_shader =
+		"#version 400\n"
+		"in vec3 vp;"
+		"void main(){"
+		"    gl_Position = vec4(vp, 1.0);"
+		"}";
+
+	const char* fragment_shader =
+		"#version 400\n"
+		"out vec4 frag_colour;"
+		"void main(){"
+		"    frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+		"}";
+
+	// Load and compile shaders
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	// Create shader program, and link
+	GLuint shader_program = glCreateProgram();
+	glAttachShader(shader_program, fs);
+	glAttachShader(shader_program, vs);
+	glLinkProgram(shader_program);
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Wipe the drawing surface
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Select shader program
+		glUseProgram(shader_program);
+
+		// Select vertex array
+		glBindVertexArray(vao);
+
+		// Draw 3 points starting from 0
+		// from the currently bound VAO with current in-use shader
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Update other events like input handling
+		glfwPollEvents();
+
+		// Put the stuff we've been drawing onto the display
+		glfwSwapBuffers(window);
+	}
 
 	// close GL context and any other GLFW resources
-	
-
-	std::string stopper = std::string();
-	std::cin >> stopper;
-
 	glfwTerminate();
 
 	return 0;
